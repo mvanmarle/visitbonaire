8bf594fc16b1ce58064199b38a119ba41fb53f1b
mvanmarle <97441731+mvanmarle@users.noreply.github.com>
2022-08-05 09:44:03 +0200

    Created project

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+import Zoutpier from "./images/Zoutpier.JPG";
+
+function Home() {
+  return (
+    <div className="home">
+      <div class="container">
+        <h1>Visit Bonaire</h1>
+        <h2>Once a visitor, always a friend</h2>
+        <img src="/images/Zoutpier.JPG" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
