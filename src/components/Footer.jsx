79c6fa77a55c2527a25fb316d6cba6279bd6d7de
mvanmarle <97441731+mvanmarle@users.noreply.github.com>
2022-08-07 18:47:07 +0200

    Styled homepage and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,84 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
-        <div class="container">
-          <p class="m-0 text-center text-white">
-            Copyright &copy; Your Website 2022
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <img
+                src="/images/Logo-VB.png"
+                alt="Visit Bonaire"
+                className="img-fluid logo"
+              />
+            </div>
+            <div className="col-sm-3">
+              <ul>
+                <li>
+                  <h6>Snel naar</h6>
+                </li>
+                <li>
+                  <a href="/">Tips voor jou</a>
+                </li>
+                <li>
+                  <a href="/">Eten en drinken</a>
+                </li>
+                <li>
+                  <a href="/">Watersporten</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-sm-3">
+              <ul>
+                <li>
+                  <h6>Praktische info</h6>
+                </li>
+                <li>
+                  <a href="/">Veelgestelde vragen</a>
+                </li>
+                <li>
+                  <a href="/">Overnachten</a>
+                </li>
+                <li>
+                  <a href="/">Auto huren</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="social-links d-flex justify-content-center">
+            <a
+              href="https://www.instagram.com/maureenvanmarle/"
+              target="_blank"
+              title="Instagram Profile"
+            >
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a
+              href="https://github.com/mvanmarle"
+              target="_blank"
+              title="GitHub Profile"
+            >
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/maureenvanmarle/"
+              target="_blank"
+              title="Linked-in Profile"
+            >
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+          <p class="text-center mt-5">
+            <a
+              href="https://github.com/mvanmarle/Portfolio-MaureenvanMarle"
+              target="_blank"
+              title="Github Project repository"
+            >
+              Open source code
+            </a>{" "}
+            by Maureen van Marle 🌸
           </p>
         </div>
       </footer>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,10 +30,10 @@ function Home() {
           <div className="col-2">
             <h3>Natuur</h3>
           </div>
-          <div className="col-2">
+          <div className="col-3">
             <h3>Watersport</h3>
           </div>
-          <div className="col-4">
+          <div className="col-3">
             <img
               src="/images/logo-VB.png"
               alt="Visit Bonaire"
@@ -45,6 +45,109 @@ function Home() {
           </div>
           <div className="col-2">
             <h3>Stad</h3>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <h4>Visit Bonaire</h4>
+        <h5>Bezoek het paradijs</h5>
+        <p>
+          Een bezoek aan Bonaire staat voor bijzondere herinneringen en
+          hartverwarmende momenten. Maar wat mag je nou echt niet missen? Wij
+          wijzen je de weg naar de leukste plekjes en verborgen parels. Ontdek
+          meer en haal alles uit je visit aan bonaire.
+        </p>
+        <div className="row images">
+          <div className="col-sm-4 p-1">
+            <img
+              src="images/Zoutpier.JPG"
+              alt="Bonaire"
+              className="img-fluid rounded"
+            />
+            <h5 className="p-2">Overnachten op bonaire</h5>
+          </div>
+          <div className="col-sm-4 p-1">
+            <img
+              src="images/Zoutpier.JPG"
+              alt="Bonaire"
+              className="img-fluid rounded"
+            />
+            <h5 className="p-2">Veelgestelde vragen</h5>
+          </div>
+          <div className="col-sm-4 p-1">
+            <img
+              src="images/Zoutpier.JPG"
+              alt="Bonaire"
+              className="img-fluid rounded"
+            />
+            <h5 className="p-2">Eten en drinken</h5>
+          </div>
+        </div>
+        <div className="row mt-4 mb-5">
+          <div className="col-sm-4 p-1">
+            <img
+              src="images/Zoutpier.JPG"
+              alt="Bonaire"
+              className="img-fluid rounded"
+            />
+            <h5 className="p-2">Uitagenda</h5>
+          </div>
+          <div className="col-sm-4 p-1">
+            <img
+              src="images/Zoutpier.JPG"
+              alt="Bonaire"
+              className="img-fluid rounded"
+            />
+            <h5 className="p-2">Tips voor jou</h5>
+          </div>
+          <div className="col-sm-4 p-1">
+            <img
+              src="images/Zoutpier.JPG"
+              alt="Bonaire"
+              className="img-fluid rounded"
+            />
+            <h5 className="p-2">Sporten op bonaire</h5>
+          </div>
+        </div>
+        <button className="primary mb-5">Ontdek meer</button>
+      </div>
+      <div className="container-fluid email pt-5 pb-5 mb-5">
+        <div className="row">
+          <div className="col-md-7 ps-5 pe-0">
+            <h4>
+              Schrijf je in voor de nieuwsbrief en blijf op de hoogte van alles
+              wat er te doen is
+            </h4>
+          </div>
+          <div className="col-md-2 mb-2">
+            <button className="primary">inschrijven</button>
+          </div>
+          <div className="col-md-3">
+            <button className="outline">Tips voor jou</button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <h4>Visit bonaire</h4>
+            <p>
+              Wil je ook graag het gevoel ervaren van volledige ontspanning op
+              je vakantie? Zemmen in de blauwste zee die je je kunt voorstellen,
+              gezellig shoppen in leuke boutiques of genieten van de prachtige
+              onderwater wereld? Of wil je liever helemaal losgaan met cocktails
+              tijdens een evenement op het strand? Bonaire heeft alles. Hippe
+              beachclubs, prachtige onderwater wereld, authentieke cultuur en
+              spannende activiteiten.{" "}
+            </p>
+            <button className="primary">Ontdek meer</button>
+          </div>
+          <div className="col-sm-6">
+            <img
+              src="images/Zoutpier.JPG"
+              alt="Bezoek Bonaire"
+              className="img-fluid rounded mt-3"
+            />
           </div>
         </div>
       </div>
